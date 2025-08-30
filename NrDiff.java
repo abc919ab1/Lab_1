4db06c3c658cc514afb46991783b242c4063ac65
Oskar <abc919ab1@gmail.com>
2025-08-30 14:49:47 +0200

    Added comments.

--- NrDiff.java
+++ NrDiff.java
@@ -1,16 +1,20 @@
 public class NrDiff {
 
     public static void main(String[] args) {
-        int[] numbers = {1, 5, 9, 10};
+        int[] numbers = {1, 5, 9, 15};
 
         int min = 1;
         int max = 1;
+
+        // the loop start by checking second position of array (5) which is bigger than max value (1), thus saving 5 to max, then continuing loop.
 
         for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] > max) {
                 max = numbers[i];
             }
         }
+
+        // print out the difference between max and min values
 
         System.out.println("Difference = " + (max - min)); // 9
     }

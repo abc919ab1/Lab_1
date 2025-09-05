5357cd73ea355e5fcffb604207f0e28335eb2481
Oskar <abc919ab1@gmail.com>
2025-09-05 23:31:39 +0200

    Changed code to handle other values in array, we dont have to assume first value in array is 1

--- NrDiff.java
+++ NrDiff.java
@@ -2,20 +2,19 @@ public class NrDiff {
 
     public static void main(String[] args) {
         int[] numbers = {1, 5, 9, 15};
+        
 
-        int min = 1;
-        int max = 1;
+        int min = numbers[0];
+        int max = numbers[0];
 
-        // the loop start by checking second position of array (5) which is bigger than max value (1), thus saving 5 to max, then continuing loop.
-
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
-            }
+        for (int num : numbers) {
+            if (num < min) min = num;
+            if (num > max) max = num;
         }
 
         // print out the difference between max and min values
 
-        System.out.println("Difference = " + (max - min)); // 9
+        System.out.println("Difference = " + (max - min));
     }
+
 }
